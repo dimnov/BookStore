8e6a1bfb34e159cc473a649d1214cce71d55ab4e
dimnov <dimnov1512@gmail.com>
2023-12-03 19:38:08 +0200

    Add Hero component

---
+++ client/src/components/hero/Hero.jsx
@@ -0,0 +1,29 @@
+import "./Hero.css";
+import hand_icon from "../Assets/hand_icon.png";
+import arrow_icon from "../Assets/arrow.png";
+import book_covers from "../Assets/book-covers.png";
+import { Link } from "react-router-dom";
+
+export default function Hero() {
+  return (
+    <div className="hero">
+      <div className="hero-left">
+        <h2>NEW ARRIVALS ONLY</h2>
+        <div>
+          <div className="hero-hand-icon">
+            <p>new</p>
+            <img src={hand_icon} alt="" />
+          </div>
+          <p>books</p>
+          <p>for everyone</p>
+        </div>
+        <Link className="latest-button" to={"/shop"}>
+          Latest Collection <img src={arrow_icon} alt="" />
+        </Link>
+      </div>
+      <div className="hero-right">
+        <img src={book_covers} alt="" />
+      </div>
+    </div>
+  );
+}
