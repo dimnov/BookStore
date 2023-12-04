91d0f5787a8a93773b1738bbb3d863afc24bac69
dimnov <dimnov1512@gmail.com>
2023-12-04 10:55:04 +0200

    Add related products component

---
+++ client/src/components/relatedProducts/RelatedProducts.jsx
@@ -0,0 +1,39 @@
+import "./RelatedProducts.css";
+import Item from "../item/Item.jsx";
+import { useEffect, useState } from "react";
+import { getBooksByCategory } from "../../services/GetProduct.js";
+
+export default function RelatedProducts(props) {
+  const [books, setBooks] = useState([]);
+  const itemsToShow = 4;
+  const { product } = props;
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      if (product.category) {
+        const data = await getBooksByCategory(itemsToShow, product.category);
+        setBooks(data);
+      }
+    };
+
+    fetchBooks();
+  }, [product.category]);
+
+  return (
+    <div className="relatedproducts">
+      <h1>Related Books</h1>
+      <hr />
+      <div className="relatedproducts-item">
+        {books.map((item, i) => (
+          <Item
+            key={i}
+            id={item.id}
+            name={item.name}
+            image={item.image}
+            price={item.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
