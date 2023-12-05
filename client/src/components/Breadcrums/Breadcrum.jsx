857a6f3ca2c9fa87ee1afb2679410411ad955479
dimnov <dimnov1512@gmail.com>
2023-12-05 17:17:50 +0200

    Update Breadcrum.jsx

--- client/src/components/Breadcrums/Breadcrum.jsx
+++ client/src/components/Breadcrums/Breadcrum.jsx
@@ -1,5 +1,5 @@
 import "./Breadcrum.css";
-import arrow_icon from "../Assets/breadcrum_arrow.png";
+import breadcrum from "../../Assets/breadcrum_arrow.png";
 import { Link } from "react-router-dom";
 export default function Breadcrum(props) {
   const { product } = props;
@@ -7,9 +7,9 @@ export default function Breadcrum(props) {
   return (
     <div className="breadcrum">
       <Link to={"/shop"}>SHOP</Link>
-      <img src={arrow_icon} alt="" />
+      <img src={breadcrum} alt="" />
       <Link to={"/shop"}>{product.category}</Link>
-      <img src={arrow_icon} alt="" />
+      <img src={breadcrum} alt="" />
       {product.name}
     </div>
   );
