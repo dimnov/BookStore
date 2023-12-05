461cf5a6225243bca1abb087b912955b83ee8bbd
dimnov <dimnov1512@gmail.com>
2023-12-06 00:31:26 +0200

    Delete FirebaseService.js

--- client/src/services/FirebaseService.js
+++
@@ -1,9 +0,0 @@
-import { getDocs, collection } from "firebase/firestore";
-import { db } from "../config/firebase.js";
-
-export const getDocumentCount = async () => {
-  const collectionRef = collection(db, 'books');
-  const querySnapshot = await getDocs(collectionRef);
-
-  return querySnapshot.size;
-};
