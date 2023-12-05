b8cbd88866ac8c7af934b9aa213875eaa479994b
dimnov <dimnov1512@gmail.com>
2023-12-05 17:19:37 +0200

    Add formInput component

---
+++ client/src/components/formInput/FormInput.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export default function FormInput(props) {
+  const [blur, setBlur] = useState(false);
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
+
+  const handleBlur = (e) => {
+    setBlur(true);
+  };
+
+  return (
+    <div>
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleBlur}
+        focused={blur.toString()}
+      />
+      <span className="error">{errorMessage}</span>
+    </div>
+  );
+}
