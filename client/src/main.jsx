ab9a08e44509ed2483de48df89ef0f6644df8804
dimnov <dimnov1512@gmail.com>
2023-12-06 00:32:04 +0200

    Add auth provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,14 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import ShopContextProvider from "./context/ShopContext.jsx";
+import { AuthProvider } from "./context/AuthProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ShopContextProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ShopContextProvider>
+  <AuthProvider>
+    <ShopContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ShopContextProvider>
+  </AuthProvider>
 );
