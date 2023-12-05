5b38d479cc3c6c0002b0a1bcb6849da4613bdccf
dimnov <dimnov1512@gmail.com>
2023-12-06 00:35:03 +0200

    Add auth service

---
+++ client/src/services/authService.js
@@ -0,0 +1,17 @@
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { auth, googleProvider } from "../config/firebase.js";
+// import { addNewUser } from "./userService.js";
+
+export const signInWithEmailAndPasswordHandler = async (email, password) => {
+  return await signInWithEmailAndPassword(auth, email, password);
+};
+
+export const signInWithGoogleHandler = async () => {
+  return await signInWithPopup(auth, googleProvider);
+};
+
+export const registerWithEmailAndPasswordHandler = async (email, password) => {
+  const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+  // await addNewUser(userCredential.user.uid, email, [], ["user"]);
+  return userCredential;
+};
