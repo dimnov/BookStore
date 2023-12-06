7e1aae7c4dbff00e83d07a5a2db8b4b708b6157b
dimnov <dimnov1512@gmail.com>
2023-12-07 00:22:53 +0200

    Add comment section in product details

--- client/src/components/commentsBox/CommentsBox.jsx
+++ client/src/components/commentsBox/CommentsBox.jsx
@@ -1,25 +1,140 @@
+import { useContext, useEffect, useState } from "react";
+import {
+  collection,
+  addDoc,
+  serverTimestamp,
+  query,
+  orderBy,
+  getDocs,
+  doc,
+  deleteDoc,
+  updateDoc,
+} from "firebase/firestore";
+import { db } from "../../config/firebase.js";
 import "./CommentsBox.css";
+import { AuthContext, AuthProvider } from "../../context/AuthProvider.jsx";
 
-export default function CommentsBox() {
+export default function CommentsBox({ bookId }) {
+  const { currentUser } = useContext(AuthContext);
+  const [user, setUser] = useState(currentUser ? currentUser.email : "");
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+  const [editingCommentId, setEditingCommentId] = useState(null);
+  const [error, setError] = useState();
+
+  const fetchComments = async () => {
+    try {
+      const commentsCollectionRef = collection(db, "books", bookId, "comments");
+      const commentsQuery = query(commentsCollectionRef, orderBy("timestamp"));
+      const querySnapshot = await getDocs(commentsQuery);
+
+      const commentsData = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+
+      setComments(commentsData);
+    } catch (error) {
+      console.error("Error fetching comments: ", error);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (comment.trim() === "") {
+      // You may display an error message or take appropriate action
+      setError("Comment cannot be empty.");
+      return;
+    }
+
+    const commentsCollectionRef = collection(db, "books", bookId, "comments");
+
+    try {
+      if (editingCommentId) {
+        // Editing an existing comment
+        const commentDocRef = doc(commentsCollectionRef, editingCommentId);
+        await updateDoc(commentDocRef, { comment, timestamp: new Date() });
+        setEditingCommentId(null);
+      } else {
+        // Adding a new comment
+        await addDoc(commentsCollectionRef, {
+          user: currentUser ? currentUser.email : "",
+          comment,
+          timestamp: new Date(),
+        });
+      }
+
+      setUser(currentUser ? currentUser.email : "");
+      setComment("");
+      fetchComments();
+    } catch (error) {
+      console.error("Error adding/editing comment: ", error);
+    }
+  };
+
+  const handleEdit = (commentId, commentText) => {
+    setEditingCommentId(commentId);
+    setComment(commentText);
+  };
+
+  const handleDelete = async (commentId) => {
+    try {
+      const commentDocRef = doc(db, "books", bookId, "comments", commentId);
+      await deleteDoc(commentDocRef);
+      fetchComments();
+    } catch (error) {
+      console.error("Error deleting comment: ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchComments();
+  }, [bookId]);
+
   return (
     <div className="commentsbox">
       <div className="commentsbox-navigator">
         <div className="commentsbox-nav-box">Comments</div>
       </div>
       <div className="commentsbox-comments">
-        <div>
-          <h4>User 1</h4>
-          <p>comment 1</p>
-        </div>
-        <div>
-          <h4>User 2</h4>
-          <p>comment 2</p>
-        </div>
-        <div>
-          <h4>User 3</h4>
-          <p>comment 4</p>
-        </div>
+        {comments.map((comment) => (
+          <div key={comment.id}>
+            <h4>{comment.user}</h4>
+            <p>{comment.comment}</p>
+            {currentUser && currentUser.email === comment.user && (
+              <div>
+                <button
+                  className="edit-btn"
+                  onClick={() => handleEdit(comment.id, comment.comment)}
+                >
+                  Edit
+                </button>
+                <button
+                  className="del-btn"
+                  onClick={() => handleDelete(comment.id)}
+                >
+                  Delete
+                </button>
+              </div>
+            )}
+          </div>
+        ))}
       </div>
+      <form onSubmit={handleSubmit}>
+        {currentUser ? (
+          <>
+            <input
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            />
+            <span>{error}</span>
+            <button type="submit">
+              {editingCommentId ? "Edit Comment" : "Add Comment"}
+            </button>
+          </>
+        ) : null}
+      </form>
     </div>
   );
 }
