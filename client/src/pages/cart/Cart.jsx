f9730759ff4639219439b9640d472530738143b7
dimnov <dimnov1512@gmail.com>
2023-12-08 22:41:46 +0200

    Update Cart

--- client/src/pages/cart/Cart.jsx
+++ client/src/pages/cart/Cart.jsx
@@ -1,10 +1,44 @@
-import CartItems from "../../components/cartItems/CartItems.jsx";
+import { useContext } from "react";
+import { ShopContext } from "../../context/ShopContext.jsx";
+import "./Cart.css";
+import CartTotal from "./CartTotal.jsx";
+import CartItemsList from "./CartItemsList.jsx";
 
 export default function Cart() {
+  const { getTotalCartAmount, cartItems, removeFromCart, allProducts } =
+    useContext(ShopContext);
+
+  const calculateShippingFee = (subtotal) => {
+    return subtotal > 0 && subtotal < 50 ? 5 : 0;
+  };
+
+  const subtotal = getTotalCartAmount();
+  const shippingFee = calculateShippingFee(subtotal);
+
+  const totalAmount = subtotal + shippingFee;
+
   return (
-    <div>
-      <CartItems />
-      {/* move everything here */}
+    <div className="cartitems">
+      <div className="cartitems-format-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Price</p>
+        <p>Quantity</p>
+        <p>Total</p>
+        <p>Remove</p>
+      </div>
+      <hr />
+
+      <CartItemsList
+        allProducts={allProducts}
+        cartItems={cartItems}
+        removeFromCart={removeFromCart}
+      />
+      <CartTotal
+        subtotal={subtotal}
+        shippingFee={shippingFee}
+        totalAmount={totalAmount}
+      />
     </div>
   );
 }
