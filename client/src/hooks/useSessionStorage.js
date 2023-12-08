338161c290b88e2e41b38c667766b78b7b9e1cee
dimnov <dimnov1512@gmail.com>
2023-12-08 22:41:34 +0200

    Update useSessionStorage.js

--- client/src/hooks/useSessionStorage.js
+++ client/src/hooks/useSessionStorage.js
@@ -1,18 +1,3 @@
-// import { useState, useEffect } from "react";
-
-// const useSessionStorage = (key, defaultValue) => {
-//   const storedValue = parseInt(sessionStorage.getItem(key)) || defaultValue;
-//   const [value, setValue] = useState(storedValue);
-
-//   useEffect(() => {
-//     sessionStorage.setItem(key, value.toString());
-//   }, [key, value]);
-
-//   return [value, setValue];
-// };
-
-// export default useSessionStorage;
-
 import { useState } from "react";
 
 const useSessionStorage = (key, initialValue) => {
