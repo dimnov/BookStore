456ba5c53f6f23a9ccb44af684a76290b3570269
dimnov <dimnov1512@gmail.com>
2023-12-08 22:36:08 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar.jsx";
-import Shop from "./pages/shop/Shop.jsx";
+import Home from "./pages/home/Home.jsx";
 import ShopCategory from "./pages/shopCategory/ShopCategory.jsx";
 import Product from "./pages/product/Product.jsx";
 import Cart from "./pages/cart/Cart.jsx";
@@ -21,9 +21,8 @@ function App() {
 
       <Routes>
         {/* Main Routes */}
-        <Route path="/" element={<Shop />} />
+        <Route path="/" element={<Home />} />
         <Route path="/shop" element={<ShopCategory />} />
-        <Route path="/search" element={<Search />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="product" element={<Product />}>
           <Route path=":productId" element={<Product />} />
@@ -32,6 +31,7 @@ function App() {
         {/* Other Routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/search" element={<Search />} />
         <Route path="*" element={<NotFound />} />
 
         {/* Protected Routes */}
