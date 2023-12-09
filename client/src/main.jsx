1aa51e4219df0cae674754d0a2f4746402d61e1c
dimnov <dimnov1512@gmail.com>
2023-12-09 23:23:40 +0200

    Update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import ShopContextProvider from "./context/ShopContext.jsx";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
