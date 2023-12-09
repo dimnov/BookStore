ef904a322adef4a564fa808eccf179586dc7d75b
dimnov <dimnov1512@gmail.com>
2023-12-09 13:17:52 +0200

    Add favorite page

--- client/src/pages/favorite/Favorite.jsx
+++ client/src/pages/favorite/Favorite.jsx
@@ -1,7 +1,50 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../context/AuthProvider.jsx";
+import Item from "../../components/item/Item.jsx";
+import { getUserFavoriteBooks } from "../../services/favoriteService.js";
+import "./Favorite.css";
+
 export default function Favorite() {
+  const [books, setBooks] = useState();
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (currentUser?.uid) {
+          const favoriteBooks = await getUserFavoriteBooks(currentUser.uid);
+          setBooks(favoriteBooks);
+        }
+      } catch (error) {
+        console.error("Error fetching favorite books:", error);
+      }
+    };
+
+    fetchData();
+  }, [currentUser?.uid]);
+
   return (
-    <div>
-      <h1>Favorite</h1>
+    <div className="favorite">
+      {books?.length > 0 ? (
+        <>
+          <h2 className="header">Your favorite books:</h2>
+          <div className="shopcategory-products">
+            {books?.map((item, i) => {
+              return (
+                <Item
+                  key={i}
+                  id={item.id}
+                  name={item.name}
+                  image={item.image}
+                  price={item.price}
+                />
+              );
+            })}
+          </div>
+        </>
+      ) : (
+        <h2 className="header">No books added.</h2>
+      )}
     </div>
   );
 }
